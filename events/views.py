0251664aa8814e766bcdeb513fec9267c9c1fe3d
Karosko dev <t.karavasilev@gmail.com>
2023-11-06 00:30:27 +0000

    created the view code,template to render the view and connect up urls for event detail

--- events/views.py
+++ events/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from django.views import generic
+from django.shortcuts import render, get_object_or_404
+from django.views import generic, View
 from .models import Event
  
 
@@ -8,3 +8,20 @@ class EventList(generic.ListView):
     queryset = Event.objects.filter(status=1).order_by('date')
     template_name = 'index.html'
     paginate_by = 6
+
+
+class EventDetail(View):
+
+    def get(self, request, slug, *args, **kwargs):
+        queryset = Event.objects.filter(status=1)
+        event = get_object_or_404(queryset, slug=slug)
+        entries = event.entries.filter(approved=True).order_by('created_on')
+
+        return render(
+            request,
+            "event_detail.html",
+            {
+                "event": event,
+                "entries": entries
+            },
+        )
