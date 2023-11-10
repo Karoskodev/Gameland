ecc0665decc12df47f872c95d13f93dd6e06379b
Karosko dev <t.karavasilev@gmail.com>
2023-11-10 00:05:49 +0000

    implement logic to restrict users to one entry per event

--- events/views.py
+++ events/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from django.views import generic, View
-from .models import Event
+from .models import Event, Entry
 from .forms import EntryForm
  
 
@@ -18,41 +18,66 @@ class EventDetail(View):
         event = get_object_or_404(queryset, slug=slug)
         entries = event.entries.filter(approved=True).order_by('created_on')
 
-        return render(
-            request,
-            "event_detail.html",
-            {
-                "event": event,
-                "entries": entries,
-                "entered": False,
-                "entry_form": EntryForm()
-            },
-        )
+        entry_exists = Entry.objects.filter(user=request.user, event=event).exists()
+
+        if not entry_exists:
+
+            return render(
+                request,
+                "event_detail.html",
+                {
+                    "event": event,
+                    "entries": entries,
+                    "entered": False,
+                    "entry_form": EntryForm()
+                },
+            )
+        else:
+            return render(
+                request,
+                "event_detail.html",
+                {
+                    "event": event,
+                    "entries": entries,
+                    "entry_exists": True,
+                },
+            )
 
     def post(self, request, slug, *args, **kwargs):
         queryset = Event.objects.filter(status=1)
         event = get_object_or_404(queryset, slug=slug)
         entries = event.entries.filter(approved=True).order_by('created_on')
 
-        entry_form = EntryForm(data=request.POST)
+        if request.user.is_authenticated:
+            # Check if the user already has an entry for the event
+            entry_exists = Entry.objects.filter(user=request.user, event=event).exists()
 
-        if entry_form.is_valid():
-            entry_form.instance.email = request.user.email
-            entry = entry_form.save(commit=False)
-            entry.event = event
-            entry.save()
-
+            # If the user doesn't have an entry, process the form
+            if not entry_exists:
+                entry_form = EntryForm(data=request.POST)
+                if entry_form.is_valid():
+                    entry_form.instance.email = request.user.email
+                    entry = entry_form.save(commit=False)
+                    entry.event = event
+                    entry.user = request.user
+                    entry.save()
+                    return render(
+                        request,
+                        "event_detail.html",
+                        {
+                            "event": event,
+                            "entries": entries,
+                            "entered": True,
+                            "entry_form": EntryForm()
+                        },
+                    )
         else:
-            entry_form = EntryForm()
-
-
-        return render(
-            request,
-            "event_detail.html",
-            {
-                "event": event,
-                "entries": entries,
-                "entered": True,
-                "entry_form": EntryForm()
-            },
-        )
+            return render(
+                request,
+                "event_detail.html",
+                {
+                    "event": event,
+                    "entries": entries,
+                    "entry_exists": True,
+                },
+            )
