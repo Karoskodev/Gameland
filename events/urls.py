1a828caef83c030903cf0666d3558b838e40c398
Karosko dev <t.karavasilev@gmail.com>
2023-11-12 17:52:08 +0000

    updadte edit_entry pa

--- events/urls.py
+++ events/urls.py
@@ -4,7 +4,7 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.EventList.as_view(), name='home'),
-    path('edit_entry/', views.EntryUpdateView.as_view(), name='edit_entry'),
+    path('edit_entry/<slug:event>/', views.EntryUpdateView.as_view(), name='edit_entry'),
     path('delete_entry/', views.EntryDeleteView.as_view(), name='delete_entry'),
     path('<slug:slug>/', views.EventDetail.as_view(), name='event_detail'),
 ]
