a53a2f440300e0984a364e3faee89b031c39b6fe
Karosko dev <t.karavasilev@gmail.com>
2023-11-14 22:01:52 +0000

    add coment for code in admin.py

--- events/admin.py
+++ events/admin.py
@@ -1,8 +1,9 @@
+# Import necessary modules from Django
 from django.contrib import admin
 from .models import Event, Entry
 from django_summernote.admin import SummernoteModelAdmin
 
-
+# Register the Event model with the admin site
 @admin.register(Event)
 class EventAdmin(SummernoteModelAdmin):
 
@@ -13,12 +14,14 @@ class EventAdmin(SummernoteModelAdmin):
     summernote_fields = ('excerpt')
 
 
+# Register the Entry model with the admin site
 @admin.register(Entry)
 class EntryAdmin(admin.ModelAdmin):
     list_display = ('user', 'nick_name', 'clan', 'event','created_on', 'approved',)
     list_filter = ('approved', 'created_on', 'event')
     search_fields = ['event', 'nick_name', 'clan']
     actions = ['approve_entries']
-
+    
     def approve_entries(self, request, queryset):
+        # Update the 'approved' field of selected entries to True
         queryset.update(approved=True)
